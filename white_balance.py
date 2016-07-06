bd9f3d7aa6586548d88935d172bfc6864a9d8d4c
mtessman <tessmanm@msoe.edu>
2016-07-06 09:52:20 -0500

    Initial Commit of white_balance

---
+++ white_balance.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import plantcv as pcv
+
+
+def white_balance(img, roi=None):
+
+    """Corrects the exposure of an image based on its histogram.
+    Inputs:
+    img - A grayscale image on which to perform the correction
+    roi - A list of 4 points (x, y, width, height) that form the rectangular ROI of the white color standard.
+            If a list of 4 points is not given, whole image will be used.
+
+    Returns:
+    img - Image after exposure correction
+    """
+    # Finds histogram of roi if valid roi is given. Otherwise, finds histogram of entire image
+    if len(roi) != 4:
+        hist = cv2.calcHist(tuple(img), [0], None, [256], [0, 256])  # Creates histogram of original image
+    else:
+        hist = cv2.calcHist(tuple(img[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]), [0], None, [256], [0, 256])
+
+    # Calculates index of maximum of histogram and finds alpha based on the peak
+    hmax = np.argmax(hist)
+    alpha = 255 / float(hmax)
+
+    # Converts values greater than hmax to 255 and scales all others by alpha
+    img = np.asarray(np.where(img <= hmax, np.multiply(alpha, img), 255), np.uint8)
+    return img
